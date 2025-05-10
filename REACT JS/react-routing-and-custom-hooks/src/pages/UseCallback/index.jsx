4bfcc92662f6c6486597835f0ed4477880fccc13
Aashi Goel <aashi25goel@gmail.com>
2025-05-10 13:54:06 +0530

    useCallback hook done

--- REACT JS/react-routing-and-custom-hooks/src/pages/UseCallback/index.jsx
+++ REACT JS/react-routing-and-custom-hooks/src/pages/UseCallback/index.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
+import Counter from './counter';
 
 function UseCallback(){
+  const [countOne , setCountOne] = useState(0);
+  const [countTwo , setCountTwo] = useState(0);
+
+  const memoriseSetCountOneFunct = useCallback(() => setCountOne(countOne + 1) , [countOne]);
+  const memoriseSetCountTwoFunct = useCallback(() => setCountTwo(countTwo + 1) , [countTwo]);
+
   return (
-    <div>index</div>
+    <div style={{textAlign : 'center'}}>
+      <h1>Use CallBack</h1>
+      <Counter countValue = {countOne} 
+      onClick = {memoriseSetCountOneFunct}/>
+      <Counter countValue = {countTwo} 
+      onClick = {memoriseSetCountTwoFunct}/>
+    </div>
   )
 }
 
