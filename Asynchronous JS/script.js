ec0c9a42995ea3863743d59a7ddb324997722089
Aashi Goel <aashi25goel@gmail.com>
2025-05-10 19:15:41 +0530

    Async js

---
+++ Asynchronous JS/script.js
@@ -0,0 +1,11 @@
+// setTimeout(callback , time_in_sec) :- setTimeout consisits of 2 things callback which is a function and time in seconds.
+
+
+console.log("hey")
+setTimeout(function(){
+    //here comes that code which you wish to eexecute after 2 sec 
+    console.log("hey 2 after 2 sec");
+} , 2000)
+console.log("hey2")
+
+//callback functions hamesha async code mein ans aane pr chalte hai
