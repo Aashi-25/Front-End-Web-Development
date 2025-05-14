3e80409865b49b4f3096141edf7fdf884534a1e0
Aashi Goel <aashi25goel@gmail.com>
2025-05-14 18:39:34 +0530

    firebase auth setup done

---
+++ REACT JS/react-firebase-auth/src/firebaseConfig.js
@@ -0,0 +1,17 @@
+import {initializeApp} from 'firebase/App';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB3ZDbTvC3xplH1GLT5s6Slf0luiUIfpKY",
+    authDomain: "react-firebase-auth-d20f9.firebaseapp.com",
+    projectId: "react-firebase-auth-d20f9",
+    storageBucket: "react-firebase-auth-d20f9.firebasestorage.app",
+    messagingSenderId: "954464416561",
+    appId: "1:954464416561:web:73346b4a18ee3e1afab610",
+    measurementId: "G-8TMVR68VKP"
+}
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export default auth;
