2fa4887e00a654144c1e7a09931fc4f276ed70b7
Aashi Goel <aashi25goel@gmail.com>
2025-05-21 10:23:10 +0530

    react with zustand setup done

---
+++ REACT JS/React-With-Zustand/src/counter-app/counter-value.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CounterValue() {
+  return (
+    <div>Counter Value is 100</div>
+  )
+}
+
+export default CounterValue;
---
+++ REACT JS/React-With-Zustand/src/counter-app/manage-counter.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ManageCounter() {
+  return (
+    <div>ManageCounter</div>
+  )
+}
+
+export default ManageCounter
